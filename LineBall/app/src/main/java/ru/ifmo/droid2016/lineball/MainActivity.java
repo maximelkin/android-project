5b3eeae17a94041045107bfbb03edcd6a5ed9cbe
Gleb Ryabchikov <glebvr97@gmail.com>
2016-12-12 16:25:39 +0300

    Make menu layout

--- LineBall/app/src/main/java/ru/ifmo/droid2016/lineball/MainActivity.java
+++ LineBall/app/src/main/java/ru/ifmo/droid2016/lineball/MainActivity.java
@@ -2,6 +2,8 @@ package ru.ifmo.droid2016.lineball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+
 import ru.ifmo.droid2016.lineball.Socket.ServerConnection;
 
 //TODO
@@ -10,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 }
