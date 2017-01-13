43e219e1a26880ba0b80ba391b76b38e90be8fe6
maxeljkin <maxelkin@list.ru>
2017-01-14 01:16:28 +0300

    sqlite3 implemented

--- server/db/sql_db.js
+++ server/db/sql_db.js
@@ -1,14 +1,20 @@
-var mongoose = require('mongoose');
-var models = require('./models');
+var sqlite = require('sqlite3').verbose();
+var db = new sqlite.Database('users');
+
 var crypto = require('crypto');
-var user = models.user;
 var config = require("./config.json");
 
-mongoose.connect(config.mongooseURL, { config: { autoIndex: false } });
+var scheme = "($id, $pass, $rate, $username)";
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(
-    console, 'connection error:'));
+db.run("CREATE TABLE if not exists users "
+    + "(id INTEGER NOT NULL PRIMARY KEY, "
+    + "pass TEXT NOT NULL, "
+    + "rate INTEGER NOT NULL, "
+    + "username TEXT NOT NULL)", function (err) {
+        if (err)
+            console.error(err);
+    });
+
 
 
 function hashing(password) {
@@ -16,39 +22,36 @@ function hashing(password) {
         update(password).digest('hex');
 }
 
-
 function createUser(androidId, pass, username, callback) {
     if (typeof (androidId) != 'string' || typeof (pass) != 'string')
         return callback(new Error("dangerous data"));
-    user.create({
-        _id: androidId,
-        pass: hashing(pass),
-        rate: config.defRate,
-        username: username
+    db.run("INSERT INTO users (id, pass, rate, username) VALUES " + scheme, {
+        $id: androidId,
+        $pass: hashing(pass),
+        $rate: config.defRate,
+        $username: username
     }, callback);
 }
 
-function getUsername(androidId, callback){
-    user.findOne({_id: androidId}, callback);
+function getUsername(androidId, callback) {
+    db.get("SELECT username FROM users WHERE id = ?", androidId, callback);
 }
 
 function deleteUser(androidId, callback) {
     if (typeof (androidId) != 'string')
         return callback(new Error('dangerous data'));
-    user.remove({ _id: androidId }, callback);
+    db.run("DELETE FROM users WHERE id = ?", androidId, callback);
 }
 
 function checkPass(androidId, pass, callback) {
-    user.find({ _id: androidId }, function (err, user) {
-        callback(err || user[0] == null || user[0].pass != hashing(pass));
+    db.get("SELECT pass FROM users WHERE id = ?", androidId, function (err, row) {
+        callback(err || row == null || row.pass != hashing(pass));
     });
 }
 
-
 function clearUsers(callback) {
-    user.update({}, { rate: config.defRate }, callback);
+    db.all("UPDATE users SET rate = ?", config.defRate, callback);
 }
-
 
 function updateRate(androidId, isWon, callback) {
     if (typeof (androidId) != 'string')
@@ -56,7 +59,7 @@ function updateRate(androidId, isWon, callback) {
     var change = -config.deltaRatingChange;
     if (isWon)
         change = config.deltaRatingChange;
-    user.update({ _id: androidId }, { rate: { $inc: change } }, callback);
+    db.run("UPDATE users SET rate = (rate + ?) WHERE id = ?", change, androidId, callback);
 }
 
 module.exports.createUser = createUser;
